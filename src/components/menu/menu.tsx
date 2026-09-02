80cd6d6828dce2e1a2db518cfd730fa57b9ae9e5
angel1cad1n1z <angelicadiniz_ad@hotmail.com>
2026-09-01 21:56:21 -0300

    cria o estado que altera o modo dark/light

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,21 +1,78 @@
 import styles from "./menu.module.css";
-// import * as React from "react";
-import { HistoryIcon, HouseIcon, Settings, SunIcon } from "lucide-react";
+import { useState, useEffect } from "react";
+import {
+  HistoryIcon,
+  HouseIcon,
+  MoonIcon,
+  Settings,
+  SunIcon,
+} from "lucide-react";
+
+type AvailableThemes = "light" | "dark";
 
 export function Menu() {
+  // Estado para armazenar o tema atual, inicializado como "dark"
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const storageTheme = localStorage.getItem(
+      "theme",
+    ) as AvailableThemes | null;
+    return storageTheme; // Se não houver tema armazenado, usa "dark" como padrão
+  });
+
+  // Define um objeto que mapeia os temas para os ícones correspondentes
+  const nextThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />,
+  };
+
+  function handleThemeChange(
+    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) {
+    e.preventDefault(); // Impede o comportamento padrão do link, que é navegar para outra página
+
+    setTheme((prevTheme) => {
+      // Alterna entre os temas "dark" e "light"
+      const newTheme = prevTheme === "dark" ? "light" : "dark";
+      return newTheme;
+    });
+  }
+
+  // Executa apenas quando o valor de theme muda
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme); // Armazena o tema atual no localStorage para persistência
+  }, [theme]);
+
   return (
     <nav className={styles.menu}>
-      <a className={styles.menuLink} href="#">
+      <a className={styles.menuLink} href="#" aria-label="Home" title="Home">
         <HouseIcon />
       </a>
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="History"
+        title="History"
+      >
         <HistoryIcon />
       </a>
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Settings"
+        title="Settings"
+      >
         <Settings />
       </a>
-      <a className={styles.menuLink} href="#">
-        <SunIcon />
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Theme"
+        title="Theme"
+        onClick={handleThemeChange}
+      >
+        {/* mostra o tema atual */}
+        {nextThemeIcon[theme]}
       </a>
     </nav>
   );
